d1c8176a38be10c0c0b644f74b98b97becb6ab56
fuhao1517 <fh2015@126.com>
2019-10-30 20:18:47 +0800

    分类完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,4 @@
+import request from "../../utils/request.js"
 // pages/category/index.js
 Page({
 
@@ -5,62 +6,36 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        list: [],
+        current: 0
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-
+    onLoad: function(options) {
+        /* 获取分类列表数据 */
+        request({
+            url: "/api/public/v1/categories"
+        }).then(res => {
+            const {
+                message
+            } = res.data
+            this.setData({
+                list: message
+            })
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    /* tab栏切换 */
+    handleClick(event) {
+        const {
+            index
+        } = event.target.dataset
+        
+        this.setData({
+            current: index
+        })
     }
+
+
 })
