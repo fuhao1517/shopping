83201daf61f7a923d6211a4ad80287eb298dbf0e
fuhao1517 <fh2015@126.com>
2019-10-31 19:52:27 +0800

    商品详情渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,70 +1,33 @@
 // pages/goods_detail/index.js
+import request from "../../utils/request.js"
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        goodsimage:[
-            "https://cdn.pinduoduo.com/home/static/img/subject/furniture.jpg",
-            "https://cdn.pinduoduo.com/home/static/img/subject/furniture.jpg",
-            "https://cdn.pinduoduo.com/home/static/img/subject/furniture.jpg"
-        ]
+
+        detail:{}
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        const {goods_id}=options
+        request({
+            url:"/api/public/v1/goods/detail",
+            data:{
+                goods_id
+            }
+        }).then(res=>{
+            const{message} =res.data
+            this.setData({
+                detail:message
+            })
+       
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+   
 })
