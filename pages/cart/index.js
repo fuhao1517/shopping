35b95da51b8e4914aa8ed401516bb3c8b7fe5504
fuhao1517 <fh2015@126.com>
2019-11-03 23:09:26 +0800

    点击全选按钮状态取反

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -11,7 +11,10 @@ Page({
         /* 是否选中 */
         selected: true,
         /* 总价格 */
-        allPrice: 0
+        allPrice: 0,
+        /* 是否全选 */
+        allSelected:true
+
     },
     /* 获取收货地址 */
     handleAddress() {
@@ -39,6 +42,8 @@ Page({
         })
         /* 计算总价格 */
         this.handleAllPrice()
+        /* 判断全选状态 */
+        this.handleAllSelected()
     },
     /* 数量加一 */
     handleAdd(event) {
@@ -146,6 +151,8 @@ Page({
         wx.setStorageSync("goods", goods)
         /* 计算总价格 */
         this.handleAllPrice()
+        /* 判断全选状态 */
+        this.handleAllSelected()
     },
     /* 封装计算总价格的方法 */
     handleAllPrice() {
@@ -165,6 +172,39 @@ Page({
         this.setData({
             allPrice: price
         })
+    },
+    /* 全选状态 */
+    handleAllSelected(){
+        const{goods}=this.data
+        let isSelect=true;
+        /* 判断是否有一个是没选中 */
+        Object.keys(goods).forEach(v=>{
+            if(!goods[v].selected){
+                isSelect=false;
+            }
+        })
+        this.setData({
+            allSelected:isSelect
+        })
+    },
+    /* 点击全选按钮的事件 */
+    handleAllSelectedEvent(){
+        console.log(123)
+        const{goods,allSelected}=this.data;
+
+        /* 循环取反选中状态，取反是根据allSelected */
+        Object.keys(goods).forEach(v=>{
+            goods[v].selected=!allSelected
+        })
+        this.setData({
+            goods,
+            /* 判断全选状态 */
+            allSelected:!allSelected
+        })
+        /* 保存到本地 */
+        wx.setStorageSync("goods",goods)
+        /* 计算总价格 */
+        this.handleAllPrice()
     }
 
 })
