03603ad2c72e54ff6fce3b204ba979d002ccdc88
fuhao1517 <fh2015@126.com>
2019-11-03 16:18:48 +0800

    把商品加入到本地购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -6,27 +6,55 @@ Page({
      * 页面的初始数据
      */
     data: {
-        detail:{}
+        detail: {}
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-        const {goods_id}=options
+    onLoad: function(options) {
+        const {
+            goods_id
+        } = options
         request({
-            url:"/api/public/v1/goods/detail",
-            data:{
+            url: "/api/public/v1/goods/detail",
+            data: {
                 goods_id
             }
-        }).then(res=>{
-            const{message} =res.data
+        }).then(res => {
+            const {
+                message
+            } = res.data
             this.setData({
-                detail:message
+                detail: message
             })
-      
         })
     },
+    /* 把商品加入到本地的购物车 */
+    handleAddcart() {
+        console.log(123)
+        /* 从本地获取购物车列表 */
+        const goods = wx.getStorageSync("goods") || {};
+        const {
+            goods_id,
+            goods_name,
+            goods_small_logo,
+            goods_price
+        } = this.data.detail
 
-   
+        goods[goods_id]={
+            goods_id,
+            goods_name,
+            goods_small_logo,
+            goods_price,
+
+            number:1,
+            selected:true
+        }
+        /* 保存到本地 */
+        wx.setStorageSync("goods",goods)
+    }
+
+
+
 })
