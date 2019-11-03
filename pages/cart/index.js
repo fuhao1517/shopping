a99f6d03a71894d36da1ca78227e8a15d8897340
fuhao1517 <fh2015@126.com>
2019-11-03 16:55:24 +0800

    渲染购物车列表

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -4,9 +4,11 @@ Page({
      * 页面的初始数据
      */
     data: {
-        goods: [],
+       
         /* 收货地址 */
-        site: {}
+        site: {},
+        /* 购物车商品列表 */
+        goods:null,
     },
 
     /* 获取收货地址 */
@@ -28,7 +30,13 @@ Page({
         })
 
     },
-    
+onShow(){
+    /* 每次打开页面时都在本地获取购物车的数据 */
+  const goods=  wx.getStorageSync("goods")||null;
+    this.setData({
+        goods
+    })
+}
 
 
 })
