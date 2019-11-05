4b768454d1b9cbec21f396efa15636ee1e0d2469
fuhao1517 <fh2015@126.com>
2019-11-05 19:34:52 +0800

    创建订单成功

--- pages/order_enter/index.js
+++ pages/order_enter/index.js
@@ -1,10 +1,11 @@
 // pages/order_enter/index.js
+import request from "../../utils/request.js"
 Page({
     /**
      * 页面的初始数据
      */
     data: {
-        site: wx.getStorageSync("site")||{},
+        site: wx.getStorageSync("site") || {},
         /* 商品信息 */
         goods: wx.getStorageSync("goods"),
         /* 总价格 */
@@ -15,11 +16,11 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-      this.setData({
-          /* 地址 */
-          site: wx.getStorageSync("site") || {},
-      }),
-          this.handleAllPrice()
+        this.setData({
+                /* 地址 */
+                site: wx.getStorageSync("site") || {},
+            }),
+            this.handleAllPrice()
     },
     /* 封装计算总价格的方法 */
     handleAllPrice() {
@@ -40,5 +41,32 @@ Page({
             allPrice: price
         })
     },
+    /* 发起支付 */
+    handlePay() {
+        const {
+            goods,
+            allPrice,
+            site
+        } = this.data
+        /* 把goods对象的值合并成数组，并且给对象添加goods_number属性 */
+        const newGoods = Object.keys(goods).map(v => {
+            goods[v].goods_number = goods[v].number;
+            return goods[v]
+        })
+        request({
+            url: "/api/public/v1/my/orders/create",
+            method: "POST",
+            data: {
+                order_price: allPrice,
+                consignee_addr: site.detail,
+                goods: newGoods
+            },
+            header: {
+                Authorization: wx.getStorageSync('token')
+            }
+        }).then(res => {
+            console.log(res)
+        })
+    }
 
 })
